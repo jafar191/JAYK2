74123e20a9b6c27eacf5cc8794650d44dad0f5b9
jafar191 <jafarhaso@gmail.com>
2025-04-12 04:04:48 +0300

    firebase-config.js

---
+++ scripts/scripts/js/firebase-config.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCcs7O6gAU5-v0FL7KokL0CmVLdyWUBdYM",
+  authDomain: "jayk-1.firebaseapp.com",
+  projectId: "jayk-1",
+  storageBucket: "jayk-1.firebasestorage.app",
+  messagingSenderId: "1093537815530",
+  appId: "1:1093537815530:web:11b7ea7b9b92182de23093",
+  measurementId: "G-VH4YEF6ZL5"
+};
+
+// تهيئة Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
